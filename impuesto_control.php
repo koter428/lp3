6e978c6aa2df467c9471448e6b402ed6b8a802b9
DMzS <domenicoenriquemeza20014@gmail.com>
2021-10-16 15:49:49 -0300

    fix(impuesto): Fix missing params & minnor errors
    This fixes the missing values and refactors some of the impuesto code.
    Was applied some file formatting and refactors.

--- impuesto_control.php
+++ impuesto_control.php
@@ -3,26 +3,28 @@
 require 'clases/conexion.php';
 
 switch ($_REQUEST['accion']) {
-    case 1://insertar
+    case 1: //insertar
         $sql = "insert into tipo_impuesto(tipo_cod,tipo_descri,tipo_porcen) "
-                . "values((select coalesce(max(tipo_cod),0)+1 from tipo_impuesto),' "
-                . $_REQUEST['vtipo_descri'] . "'," . $_REQUEST['vtipo_porcen'] . ")";
+            . "values((select coalesce(max(tipo_cod),0)+1 from tipo_impuesto),'"
+            . $_REQUEST['vtipo_descri'] . "'," . $_REQUEST['vtipo_porcen'] . ")";
         $mensaje = "Se guardo correctamente";
         break;
 
-    case 2://actualizar
+    case 2: //actualizar
         $sql = "update tipo_impuesto set tipo_descri = '" . $_REQUEST['vtipo_descri']
-                . "',tipo_porcen='" . $_REQUEST['vtipo_porcen']
-                . "' where tipo_cod =" . $_REQUEST['vtipo_cod'];
+            . "', tipo_porcen='" . $_REQUEST['vtipo_porcen']
+            . "' where tipo_cod ='" . $_REQUEST['vtipo_cod']
+            . "'";
         $mensaje = "Se actualizo correctamente";
         break;
-    case 3://borrar
+
+    case 3: //borrar
         $sql = "UPDATE articulo "
-                . "SET tipo_cod=0 "
-                . "WHERE tipo_cod='"
-                . $_REQUEST['vtipo_cod']."';"
-                . "delete from tipo_impuesto where tipo_cod='"
-                . $_REQUEST['vtipo_cod']. "'";
+            . "SET tipo_cod=0 "
+            . "WHERE tipo_cod='"
+            . $_REQUEST['vtipo_cod'] . "';"
+            . "delete from tipo_impuesto where tipo_cod='"
+            . $_REQUEST['vtipo_cod'] . "'";
         $mensaje = "Se borro correctamente";
         break;
 }
