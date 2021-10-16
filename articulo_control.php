e0fcb779a643ef8e88cf1705e7574beb3fec1863
DMzS <domenicoenriquemeza20014@gmail.com>
2021-10-16 15:57:35 -0300

    fix!(articulo): Minnor Fixes & Code Formatting
    NOTE: There are some errors within the articulo_control, articulo_edit
    and the articulo_index.

--- articulo_control.php
+++ articulo_control.php
@@ -4,42 +4,40 @@ session_start();
 
 switch ($_REQUEST['accion']) {
     case 1:
-        $sql = "insert into clientes(cli_cod,cli_ci,cli_nombre,cli_apellido,cli_telefono,cli_direcc) "
-            . "values((select coalesce(max(cli_cod),0)+1 from clientes),"
-            .$_REQUEST['ar_cod'].",'"
-            .$_REQUEST['art_precioc']."','"
-            .$_REQUEST['art_preciov']."','"
-            .$_REQUEST['art_descri']."','"
-            .$_REQUEST['tipo_cod']."')"
-            .$_REQUEST['tipo_cod']."')";
-    
-        $mensaje='Se inserto correctamente el cliente';
+        $sql = "insert into articulo"
+            . "values((select coalesce(max(art_cod),0)+1 from articulo), '"
+            . $_REQUEST['art_precioc'] . "','"
+            . $_REQUEST['art_preciov'] . "','"
+            . $_REQUEST['art_descri'] . "','"
+            . $_REQUEST['tipo_cod'] . "')";
+
+        $mensaje = 'Se inserto correctamente el cliente';
         break;
 
     case 2: //actualizar
-        $sql="update clientes set cli_ci = '".$_REQUEST['vcli_ci']
-            ."',cli_nombre='".$_REQUEST['vcli_nombre']
-            ."',cli_apellido='".$_REQUEST['vcli_apellido']
-            ."',cli_telefono='".$_REQUEST['vcli_telefono']
-            ."',cli_direcc='".$_REQUEST['vcli_direcc']
-            ."' where cli_cod=".$_REQUEST['vcli_cod'];
-            $mensaje='Se actualizo correctamente';
+        $sql = "update clientes set cli_ci = '" . $_REQUEST['vcli_ci']
+            . "',cli_nombre='" . $_REQUEST['vcli_nombre']
+            . "',cli_apellido='" . $_REQUEST['vcli_apellido']
+            . "',cli_telefono='" . $_REQUEST['vcli_telefono']
+            . "',cli_direcc='" . $_REQUEST['vcli_direcc']
+            . "' where cli_cod=" . $_REQUEST['vcli_cod'];
+        $mensaje = 'Se actualizo correctamente';
         break;
     case 3: //borrar
-            $sql="delete from clientes where cli_cod ='".$_REQUEST['vcli_cod']
-            ."',cli_nombre='".$_REQUEST['vcli_nombre']
-            ."',cli_apellido='".$_REQUEST['vcli_apellido']
-            ."',cli_telefono= '".$_REQUEST['vcli_telefono']
-            ."',cli_direcc='".$_REQUEST['vcli_direcc']
-            ."' where cli_cod =".$_REQUEST['cli_cod'];
-            $mensaje = "Se borro correctamente";
+        $sql = "delete from clientes where cli_cod ='" . $_REQUEST['vcli_cod']
+            . "',cli_nombre='" . $_REQUEST['vcli_nombre']
+            . "',cli_apellido='" . $_REQUEST['vcli_apellido']
+            . "',cli_telefono= '" . $_REQUEST['vcli_telefono']
+            . "',cli_direcc='" . $_REQUEST['vcli_direcc']
+            . "' where cli_cod =" . $_REQUEST['cli_cod'];
+        $mensaje = "Se borro correctamente";
         break;
 }
 
 if (consultas::ejecutar_sql($sql)) {
     $_SESSION['mensaje'] = $mensaje;
     header("location:cliente_index.php");
-}else{
-    $_SESSION['mensaje'] = "Error al procesar ".$sql;
-    header("location:cliente_index.php");    
+} else {
+    $_SESSION['mensaje'] = "Error al procesar " . $sql;
+    header("location:cliente_index.php");
 }
--- articulo_index.php
+++ articulo_index.php
@@ -1,130 +1,133 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link rel="shortcut icon" type="image/x-icon" href="/lp3/img/user.png">
-        <title>LP3</title>
-        <!-- Tell the browser to be responsive to screen width -->
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-        <?php 
-        session_start();/*Reanudar sesion*/
-        require 'menu/css_lte.ctp'; ?><!--ARCHIVOS CSS-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" type="image/x-icon" href="/lp3/img/user.png">
+    <title>LP3</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-    </head>
-    <body class="hold-transition skin-blue sidebar-mini">
-        <div class="wrapper">
-            <?php require 'menu/header_lte.ctp'; ?><!--CABECERA PRINCIPAL-->
-            <?php require 'menu/toolbar_lte.ctp';?><!--MENU PRINCIPAL-->
-            <div class="content-wrapper">
-                <!-- CONTENEDOR PRINCIPAL -->
-                <div class="content">
-                    <!-- FILA 1 -->
-                    <div class="row">
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <?php if (!empty($_SESSION['mensaje'])) { ?>
+    <?php
+    session_start();/*Reanudar sesion*/
+    require 'menu/css_lte.ctp'; ?>
+    <!--ARCHIVOS CSS-->
+
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+        <?php require 'menu/header_lte.ctp'; ?>
+        <!--CABECERA PRINCIPAL-->
+        <?php require 'menu/toolbar_lte.ctp'; ?>
+        <!--MENU PRINCIPAL-->
+        <div class="content-wrapper">
+            <!-- CONTENEDOR PRINCIPAL -->
+            <div class="content">
+                <!-- FILA 1 -->
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <?php if (!empty($_SESSION['mensaje'])) { ?>
                             <div class="alert alert-danger" id="mensaje">
                                 <span class="glyphicon glyphicon-info-sign"></span>
                                 <?php echo $_SESSION['mensaje'];
-                                    $_SESSION['mensaje'] = '';
+                                $_SESSION['mensaje'] = '';
                                 ?>
                             </div>
-                            <?php } ?>
-                            <div class="box box-primary">
-                                <div class="box-header">
-                                    <i class="ion ion-clipboard"></i>
-                                    <h3 class="box-title">Articulos</h3>
-                                    <div class="box-tools">
-                                        <a href="articulo_add.php" class="btn btn-primary btn-sm" data-title='Agregar' rel='tooltip' data-placement='top'>
-                                            <i class="fa fa-plus"></i></a>
-                                        <a href="articulo_print.php." class="btn btn-default btn-sm" data-title='Imprimir' rel='tooltip' data-placement='top' target="_blank">
-                                            <i class="fa fa-print"></i></a>
-                                    </div>
+                        <?php } ?>
+                        <div class="box box-primary">
+                            <div class="box-header">
+                                <i class="ion ion-clipboard"></i>
+                                <h3 class="box-title">Articulos</h3>
+                                <div class="box-tools">
+                                    <a href="articulo_add.php" class="btn btn-primary btn-sm" data-title='Agregar' rel='tooltip' data-placement='top'>
+                                        <i class="fa fa-plus"></i></a>
+                                    <a href="articulo_print.php." class="btn btn-default btn-sm" data-title='Imprimir' rel='tooltip' data-placement='top' target="_blank">
+                                        <i class="fa fa-print"></i></a>
                                 </div>
-                                <div class="box-body">
-                                    <div class="row">
-                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                            <form action="articulo_edit.php" method="post" accept-charset="utf-8" class="form-horizontal">
-                                                  <div class="box-body">
-                                                    <div class="form-group">
-                                                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                                      <div class="input-group custom-search-form">
-                                                        <input type="search" class="form-control" name="buscar" 
-                                                           placeholder="Ingrese el valor a buscar..." autofocus=""/>
+                            </div>
+                            <div class="box-body">
+                                <div class="row">
+                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                        <form action="articulo_edit.php" method="post" accept-charset="utf-8" class="form-horizontal">
+                                            <div class="box-body">
+                                                <div class="form-group">
+                                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                                        <div class="input-group custom-search-form">
+                                                            <input type="search" class="form-control" name="buscar" placeholder="Ingrese el valor a buscar..." autofocus="" />
                                                             <span class="input-group-btn">
-                                                          <button type="submit" class="btn btn-primary btn-flat" data-title="Buscar"
-                                                           rel="tooltip"><i class="fa fa-search"></i></button>
-                                                        </span>
-                                                      </div>
+                                                                <button type="submit" class="btn btn-primary btn-flat" data-title="Buscar" rel="tooltip"><i class="fa fa-search"></i></button>
+                                                            </span>
+                                                        </div>
                                                     </div>
-                                                   </div>
-                                                 </div> 
-                                               </form>
-                                            <?php 
-                                            //consulta a la tabla marca
-                                            $articulos = consultas::get_datos("select * from v_articulo where art_descri ilike '%".(isset($_REQUEST['buscar'])?$_REQUEST['buscar']:"")."%' order by art_cod");
-                                            //var_dump($articulos);
-                                            if (!empty($articulos)) { ?>
+                                                </div>
+                                            </div>
+                                        </form>
+                                        <?php
+                                        //consulta a la tabla marca
+                                        $articulos = consultas::get_datos("select * from v_articulo where art_descri ilike '%" . (isset($_REQUEST['buscar']) ? $_REQUEST['buscar'] : "") . "%' order by art_cod");
+                                        if (!empty($articulos)) { ?>
                                             <div class="table-responsive">
                                                 <table class="table table-condensed table-striped table-hover">
                                                     <thead>
                                                         <tr>
+                                                            <th>Marca</th>
                                                             <th>Descripcion</th>
-                                                            <th>Precio costo</th>
-                                                            <th>Precio venta</th>
+                                                            <th>Precio Compra</th>
+                                                            <th>Precio Venta</th>
                                                             <th>Impuesto</th>
                                                             <th class="text-center">Acciones</th>
                                                         </tr>
                                                     </thead>
                                                     <tbody>
                                                         <?php foreach ($articulos as $art) { ?>
-                                                        <tr>
-                                                            <td data-title='Descripción'><?php echo $art['art_cod']." ".$art['art_cod'];?></td>
-                                                            <td data-title='Precio Costo'><?php echo number_format($art['art_precioc'],0,",",".");?></td>
-                                                            <td data-title='Precio Venta'><?php echo number_format($art['art_preciov'],0,",",".");?></td>
-                                                              <td data-title='Impuesto'><?php echo $art['tipo_descri'];?></td>
-                                                            <td data-title='Acciones' class="text-center">
-                                                                <a href="articulo_edit.php?art_cod=<?php echo $art['art_cod'];?>" class="btn btn-warning btn-sm" role='button'
-                                                                   data-title='Editar' rel='tooltip' data-placement='top'>
-                                                                    <span class="glyphicon glyphicon-edit"></span>
-                                                                </a>
-                                                                <a href="articulo_del.php?vart_cod=<?php echo $art['art_cod'];?>" class="btn btn-danger btn-sm" role='button'
-                                                                   data-title='Borrar' rel='tooltip' data-placement='top'>
-                                                                    <span class="glyphicon glyphicon-trash"></span>
-                                                                </a>                                                                        
-                                                            </td>
-                                                        </tr>
+                                                            <tr>
+                                                                <td data-title='Descripción Marca'><?php echo $art['mar_descri']; ?></td>
+                                                                <td data-title='Descripción'><?php echo $art['art_descri']; ?></td>
+                                                                <td data-title='Precio Compra'><?php echo number_format($art['art_precioc'], 0, ",", "."); ?></td>
+                                                                <td data-title='Precio Venta'><?php echo number_format($art['art_preciov'], 0, ",", "."); ?></td>
+                                                                <td data-title='Impuesto'><?php echo $art['tipo_descri']; ?></td>
+                                                                <td data-title='Acciones' class="text-center">
+                                                                    <a href="articulo_edit.php?vart_cod=<?php echo $art['art_cod']; ?>" class="btn btn-warning btn-sm" role='button' data-title='Editar' rel='tooltip' data-placement='top'>
+                                                                        <span class="glyphicon glyphicon-edit"></span>
+                                                                    </a>
+                                                                    <a href="articulo_del.php?vart_cod=<?php echo $art['art_cod']; ?>" class="btn btn-danger btn-sm" role='button' data-title='Borrar' rel='tooltip' data-placement='top'>
+                                                                        <span class="glyphicon glyphicon-trash"></span>
+                                                                    </a>
+                                                                </td>
+                                                            </tr>
                                                         <?php } ?>
                                                     </tbody>
                                                 </table>
                                             </div>
-                                            <?php }else { ?>
+                                        <?php } else { ?>
                                             <div class="alert alert-info flat">
-                                                <span class="glyphicon glyphicon-info-sign"></span> 
+                                                <span class="glyphicon glyphicon-info-sign"></span>
                                                 No se han registrado Articulos...
                                             </div>
-                                            <?php }
-                                            ?>
-                                        </div>
+                                        <?php }
+                                        ?>
                                     </div>
                                 </div>
                             </div>
-                        </div>                        
+                        </div>
                     </div>
-                    <!-- FIN FILA 1 -->
-                </div>   
-                <!-- FIN CONTENEDOR PRINCIPAL -->
+                </div>
+                <!-- FIN FILA 1 -->
             </div>
-                  <?php require 'menu/footer_lte.ctp'; ?><!--ARCHIVOS JS-->  
-            </div>                  
-        <?php require 'menu/js_lte.ctp'; ?><!--ARCHIVOS JS-->
-        <script>
-            $("#mensaje").delay(4000).slideUp(200,function(){
-                $(this).alert('close');
-            })
-        </script>
-    </body>
+            <!-- FIN CONTENEDOR PRINCIPAL -->
+        </div>
+        <?php require 'menu/footer_lte.ctp'; ?>
+        <!--ARCHIVOS JS-->
+    </div>
+    <?php require 'menu/js_lte.ctp'; ?>
+    <!--ARCHIVOS JS-->
+    <script>
+        $("#mensaje").delay(4000).slideUp(200, function() {
+            $(this).alert('close');
+        })
+    </script>
+</body>
+
 </html>
-
-
