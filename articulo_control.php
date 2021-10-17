bf7c5e9f1109ede93ee6dc28f124f88a727f09ed
DMzS <domenicoenriquemeza20014@gmail.com>
2021-10-17 13:10:14 -0300

    fix(articulo): Fix CRUD operations

--- articulo_control.php
+++ articulo_control.php
@@ -4,40 +4,39 @@ session_start();
 
 switch ($_REQUEST['accion']) {
     case 1:
-        $sql = "insert into articulo"
+        $sql = "insert into articulo "
             . "values((select coalesce(max(art_cod),0)+1 from articulo), '"
-            . $_REQUEST['art_precioc'] . "','"
-            . $_REQUEST['art_preciov'] . "','"
-            . $_REQUEST['art_descri'] . "','"
-            . $_REQUEST['tipo_cod'] . "')";
+            . $_REQUEST['vart_codbarra'] . "','"
+            . $_REQUEST['vmar_cod'] . "','"
+            . $_REQUEST['vart_descri'] . "','"
+            . $_REQUEST['vart_precioc'] . "','"
+            . $_REQUEST['vart_preciov'] . "','"
+            . $_REQUEST['vtipo_cod'] . "')";
 
         $mensaje = 'Se inserto correctamente el cliente';
         break;
 
     case 2: //actualizar
-        $sql = "update clientes set cli_ci = '" . $_REQUEST['vcli_ci']
-            . "',cli_nombre='" . $_REQUEST['vcli_nombre']
-            . "',cli_apellido='" . $_REQUEST['vcli_apellido']
-            . "',cli_telefono='" . $_REQUEST['vcli_telefono']
-            . "',cli_direcc='" . $_REQUEST['vcli_direcc']
-            . "' where cli_cod=" . $_REQUEST['vcli_cod'];
+        $sql = "update articulo set art_codbarra = '" . $_REQUEST['vart_codbarra']
+            . "', mar_cod='" . $_REQUEST['vmar_cod']
+            . "', art_descri='" . $_REQUEST['vart_descri']
+            . "', art_precioc='" . $_REQUEST['vart_precioc']
+            . "', art_preciov='" . $_REQUEST['vart_preciov']
+            . "', tipo_cod='" . $_REQUEST['vtipo_cod']
+            . "' where art_cod=" . $_REQUEST['vart_cod'];
         $mensaje = 'Se actualizo correctamente';
         break;
+
     case 3: //borrar
-        $sql = "delete from clientes where cli_cod ='" . $_REQUEST['vcli_cod']
-            . "',cli_nombre='" . $_REQUEST['vcli_nombre']
-            . "',cli_apellido='" . $_REQUEST['vcli_apellido']
-            . "',cli_telefono= '" . $_REQUEST['vcli_telefono']
-            . "',cli_direcc='" . $_REQUEST['vcli_direcc']
-            . "' where cli_cod =" . $_REQUEST['cli_cod'];
+        $sql = "delete from articulo where art_cod ='" . $_REQUEST['vart_cod'] . "'";
         $mensaje = "Se borro correctamente";
         break;
 }
 
 if (consultas::ejecutar_sql($sql)) {
     $_SESSION['mensaje'] = $mensaje;
-    header("location:cliente_index.php");
+    header("location:articulo_index.php");
 } else {
     $_SESSION['mensaje'] = "Error al procesar " . $sql;
-    header("location:cliente_index.php");
+    header("location:articulo_index.php");
 }
--- articulo_del.php
+++ articulo_del.php
@@ -5,64 +5,119 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link rel="shortcut icon" type="image/x-icon" href="/lp3/favicon.ico">
-        <title>LP3</title>
-        <!-- Tell the browser to be responsive to screen width -->
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-        <?php 
-        session_start();/*Reanudar sesion*/
-        require 'menu/css_lte.ctp'; ?><!--ARCHIVOS CSS-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" type="image/x-icon" href="/lp3/favicon.ico">
+    <title>LP3</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-    </head>
-    <body class="hold-transition skin-blue sidebar-mini">
-        <div class="wrapper">
-            <?php require 'menu/header_lte.ctp'; ?><!--CABECERA PRINCIPAL-->
-            <?php require 'menu/toolbar_lte.ctp';?><!--MENU PRINCIPAL-->
-            <div class="content-wrapper">
-                <div class="content">
-                    <div class="row">
-                        <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-                            <div class="box box-danger">
-                                <div class="box-header">
-                                    <i class="fa fa-trash"></i>
-                                    <h3 class="box-title">Borrar Articulo</h3>
-                                    <div class="box-tools">
-                                        <a href="articulo_index.php" class="btn btn-primary btn-sm" data-title="Volver" >
-                                            <i class="fa fa-arrow-left"></i>
-                                        </a>
+    <?php
+    session_start();/*Reanudar sesion*/
+    require 'menu/css_lte.ctp'; ?>
+    <!--ARCHIVOS CSS-->
+
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+    <div class="wrapper">
+        <?php require 'menu/header_lte.ctp'; ?>
+        <!--CABECERA PRINCIPAL-->
+        <?php require 'menu/toolbar_lte.ctp'; ?>
+        <!--MENU PRINCIPAL-->
+        <div class="content-wrapper">
+            <div class="content">
+                <div class="row">
+                    <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
+                        <div class="box box-danger">
+                            <div class="box-header">
+                                <i class="fa fa-trash"></i>
+                                <h3 class="box-title">Borrar Articulo</h3>
+                                <div class="box-tools">
+                                    <a href="articulo_index.php" class="btn btn-primary btn-sm" data-title="Volver">
+                                        <i class="fa fa-arrow-left"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <form action="articulo_control.php" method="post" accept-charset="utf-8" class="form-horizontal">
+                                <div class="box-body">
+                                    <?php
+                                    $resultado = consultas::get_datos("select * from articulo where art_cod='" . $_GET['vart_cod'] . "'");
+                                    $marcas = consultas::get_datos("SELECT * FROM marca ORDER BY CASE WHEN mar_cod='" . $resultado[0]["mar_cod"] . "' THEN 1 ELSE 2 END, mar_cod;");
+                                    $impuestos = consultas::get_datos("SELECT * FROM tipo_impuesto ORDER BY CASE WHEN tipo_cod='" . $resultado[0]["tipo_cod"] . "' THEN 1 ELSE 2 END, tipo_cod;");
+                                    ?>
+                                    <div class="form-group">
+                                        <input type="hidden" name="accion" value="3" />
+                                        <input type="hidden" name="vart_cod" value="<?php echo $resultado[0]['art_cod'] ?>" />
                                     </div>
-                                </div> 
-                                <form action="articulo_control.php" method="post" accept-charset="utf-8" class="form-horizontal">
-                                    <div class="box-body">
-                                        <?php $resultado = consultas::get_datos("select * from clientes where cli_cod=".$_GET['vcli_cod']);?>
-                                        <div class="form-group">
-                                            <input type="hidden" name="accion" value="3"/>
-                                            <input type="hidden" name="vcli_cod" value="<?php echo $resultado[0]['cli_cod']?>"/>
-                                            <label class="control-label col-lg-2 col-md-2 col-sm-2"> Nombres:</label>
-                                            <div class="col-lg-8 col-md-8 col-sm-8">
-                                                <input type="text" name="vcli_nombre" class="form-control" required="" disabled="" 
-                                                       value="<?php echo $resultado[0]['cli_nombre']?>"/>
+                                    <div class="form-group">
+                                        <label class="col-lg-2 control-label">Descripcion:</label>
+                                        <div class="col-lg-6">
+                                            <input type="text" disabled name="vart_descri" class="form-control" required="" value="<?php echo $resultado[0]['art_descri'] ?>" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-lg-2 control-label">Marca:</label>
+                                        <div class="col-lg-6">
+                                            <div class="input-group">
+                                                <select name="vmar_cod" disabled class="custom-select" id="inputGroupSelect04">
+                                                    <?php foreach ($marcas as $marca) { ?>
+                                                        <option value="<?php echo $marca["mar_cod"] ?>"><?php echo $marca["mar_descri"] ?></option>
+                                                    <?php } ?>
+                                                </select>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="box-footer">
-                                        <button type="reset" class="btn btn-default" data-title="Cancelar" > 
-                                            <i class="fa fa-remove"></i> Cancelar</button>                                        
-                                        <button type="submit" class="btn btn-danger pull-right" data-title="Guardar" > 
-                                            <i class="fa fa-trash"></i> Borrar</button>
+                                    <div class="form-group">
+                                        <label class="col-lg-2 control-label">Impuesto:</label>
+                                        <div class="col-lg-6">
+                                            <div class="input-group">
+                                                <select name="vtipo_cod" disabled class="custom-select" id="inputGroupSelect04">
+                                                    <?php foreach ($impuestos as $impuesto) { ?>
+                                                        <option value="<?php echo $impuesto["tipo_cod"] ?>"><?php echo $impuesto["tipo_descri"] ?></option>
+                                                    <?php } ?>
+                                                </select>
+                                            </div>
+                                        </div>
                                     </div>
-                                </form>
-                            </div>
+                                    <div class="form-group">
+                                        <label class="col-lg-2 control-label">Codigo Barras:</label>
+                                        <div class="col-lg-6">
+                                            <input type="text" name="vart_codbarra" disabled class="form-control" required="" value="<?php echo $resultado[0]['art_codbarra'] ?>" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-lg-2 control-label">Precio Compra:</label>
+                                        <div class="col-lg-6">
+                                            <input type="text" name="vart_precioc" disabled class="form-control" required="" value="<?php echo $resultado[0]['art_precioc'] ?>" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="col-lg-2 control-label">Precio Venta:</label>
+                                        <div class="col-lg-6">
+                                            <input type="text" disabled name="vart_preciov" class="form-control" required="" value="<?php echo $resultado[0]['art_preciov'] ?>" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-footer">
+                                    <button type="reset" class="btn btn-default" data-title="Cancelar">
+                                        <i class="fa fa-remove"></i> Cancelar</button>
+                                    <button type="submit" class="btn btn-danger pull-right" data-title="Guardar">
+                                        <i class="fa fa-trash"></i> Borrar</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-                  <?php require 'menu/footer_lte.ctp'; ?><!--ARCHIVOS JS-->  
-            </div>                  
-        <?php require 'menu/js_lte.ctp'; ?><!--ARCHIVOS JS-->
-    </body>
+        </div>
+        <?php require 'menu/footer_lte.ctp'; ?>
+        <!--ARCHIVOS JS-->
+    </div>
+    <?php require 'menu/js_lte.ctp'; ?>
+    <!--ARCHIVOS JS-->
+</body>
+
 </html>
